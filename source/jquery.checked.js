d2fc8d8441471ae21f4c874f97efc2eddc7a43e1
Jensen Tonne <me@jstonne.com>
2012-02-17 11:54:58 +0800

    Added jquery.checked.

---
+++ source/jquery.checked.js
@@ -0,0 +1,44 @@
+/*!
+ * jquery.checked.
+ * Checked/unchecked event handler for checkbox & radio button.
+ *
+ * Copyright (c) 2012 Jensen Tonne
+ * www.jstonne.com
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+$.fn.checked = function(checked, unchecked)
+{
+	// Return checked value if no arguments are given;
+	if (arguments.length < 1)
+		return this.is(':checked');
+
+	this.each(function(i)
+	{
+		var input = $(this);
+
+		if (typeof checked == "boolean")
+		{
+			input.attr('checked', checked).trigger('change');
+			return;
+		}
+
+		if (input.is('input[type=checkbox]') || input.is('input[type=radio]'))
+		{
+			input
+				.unbind('change.checked')
+				.bind('change.checked', function()
+				{
+					try {
+						return (input.is(':checked')) ? checked.apply(input) : unchecked.apply(input);
+					} catch(e) {};
+				});
+		}
+	});
+
+	return this;
+}
